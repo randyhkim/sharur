199d618c7447e488636e069587ce4b77666b9c35
Justin <sdehv1@hotmail.com>
2022-10-22 05:54:47 -0500

    Fix join and leave and added ping for debugging.

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import os
-
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -7,20 +6,33 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents=discord.Intents.default())
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
+
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    response = "connected to Discord!"
+    print(f'{bot.user} has connected to Discord!')
 
-client.run(TOKEN)
-
-bot = commands.Bot(command_prefix = '=')
 
 @bot.command()
 async def join(ctx):
-    channel = ctx.author.VoiceState.channel
-    await channel.connect
+    channel = ctx.author.voice.channel
+    print('it probably works')
+    await channel.connect()
+
+
 @bot.command()
 async def leave(ctx):
+    print('it probably works2')
     await ctx.voice_client.disconnect()
+
+
+@bot.command()
+async def ping(ctx):
+    print('it has to work!')
+    await ctx.channel.send("pong")
+
+
+bot.run(TOKEN)
