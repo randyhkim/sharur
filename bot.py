d0a9b75d27759c1e21e25faa1817cd1ee0898f43
Justin <sdehv1@hotmail.com>
2022-10-22 03:57:13 -0500

    Import commands, and add commands to join vc.

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,3 +14,13 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
+
+bot = commands.Bot(command_prefix = '=')
+
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.VoiceState.channel
+    await channel.connect
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
